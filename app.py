061195e21a0378adebd7a67de9e3fecdca0a3f63
jrhauser <jrhauser1127@gmail.com>
2022-06-19 07:34:35 -0400

    whoops

--- app.py
+++ app.py
@@ -1,8 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g, session, redirect
+from flask_session import Session
 from cs50 import SQL
+from functools import wraps;
+from werkzeug.security import check_password_hash, generate_password_hash
+import os
+
+#FROM FINANCE ----------------------------------------------------------------
 app = Flask(__name__)
 
-db=SQL("sqlite:///MDCC.db")
+app.config["TEMPLATES_AUTO_RELOAD"]
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# https://cs50.readthedocs.io/heroku/
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgres://")
+db = SQL(uri)
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+#---------------------------------------------------------------------------------
 
 @app.route("/")
 def home():
@@ -24,8 +63,16 @@ def memorial():
 def links():
     return render_template("links.html")
 
-@app.route("/discussion", methods=["GET", "POST"])
-def discussion():
-    if request.method == "GET":
-        return render_template("discussion.html")
-    
+@app.route("/announcements")
+def announcement():
+    return render_template("announcements.html")
+
+@app.route("/newAnnouncement", methods=["GET", "POST"])
+@login_required
+def admin():
+        return render_template("newAnnouncement.html")
+
+@app.route("/delete", methods=['POST'])
+@login_required
+def delete():
+    return redirect("/announcements")
